29698442aac7c0aebc36c759d9628a0afb098727
Nischal Shakya <54927344+nischalshakya95@users.noreply.github.com>
2022-11-06 11:15:17 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          DevOPS Assignment Completed :) 
         </a>
       </header>
     </div>
